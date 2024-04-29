ff65df3e45433dfb9d0bd1b2fc3131d23a185436
Zafar4774 <zafchikdjurayev@gmail.com>
2024-04-29 15:39:46 +0500

    second

--- apiparsing/settings.py
+++ apiparsing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9pkler*bk)%_@w6mrse+@cb$@h35fi#m##dk(=#*(86lo!8s1^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
